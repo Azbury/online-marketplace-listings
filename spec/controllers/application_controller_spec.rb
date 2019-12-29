2f05c9aa3735826cd004e08c79660bccdc5bc84c
Austin Asbury <acasbury25@aol.com>
2019-12-29 16:18:24 +0000

    adding signup page tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -10,4 +10,64 @@ describe ApplicationController do
     end
   end
 
+  describe "Signup Page" do
+
+    it 'loads the signup page' do
+      get '/signup'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'signup directs user to twitter index' do
+      params = {
+        :username => "skittles123",
+        :email => "skittles@aol.com",
+        :password => "rainbows"
+      }
+      post '/signup', params
+      expect(last_response.location).to include("/items")
+    end
+
+    it 'does not let a user sign up without a username' do
+      params = {
+        :username => "",
+        :email => "skittles@aol.com",
+        :password => "rainbows"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a user sign up without an email' do
+      params = {
+        :username => "skittles123",
+        :email => "",
+        :password => "rainbows"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a user sign up without a password' do
+      params = {
+        :username => "skittles123",
+        :email => "skittles@aol.com",
+        :password => ""
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a logged in user view the signup page' do
+      #user = User.create(:username => "skittles123", :email => "skittles@aol.com", :password => "rainbows")
+      params = {
+        :username => "skittles123",
+        :email => "skittles@aol.com",
+        :password => "rainbows"
+      }
+      post '/signup', params
+      get '/signup'
+      expect(last_response.location).to include('/items')
+    end
+  end
+
 end
