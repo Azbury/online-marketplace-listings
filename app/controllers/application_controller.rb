7710c87aca81a17c715b42dda6a98d9b12a49548
Austin Asbury <acasbury25@aol.com>
2019-12-29 18:43:39 +0000

    adding login to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,4 +31,22 @@ class ApplicationController < Sinatra::Base
       redirect '/items'
     end
   end
+
+  get '/login' do
+    if session[:user_id] != nil
+      redirect '/items'
+    else
+      erb :login
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/items"
+    else
+      redirect "/login"
+    end
+  end
 end
